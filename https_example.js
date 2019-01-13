8794e374c831f2437a45a55391ddab610c4211d3
Clive <seelive.i@gmail.com>
2019-01-13 22:41:27 +0000

    added chunk and data to call

--- https_example.js
+++ https_example.js
@@ -8,7 +8,10 @@ const options = {
 // called by https when the request is made.
 const callback = function (response) {
   console.log('In response handler callback!');
-  console.log('Response: ', response);
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 };
 
 console.log("I'm about to make the request!");
