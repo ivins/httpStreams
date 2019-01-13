e9100f9c9c801215059d3e54599cc9e3574c86d9
Clive <seelive.i@gmail.com>
2019-01-13 22:35:24 +0000

    added response argument to callback function

--- https_example.js
+++ https_example.js
@@ -6,8 +6,9 @@ const options = {
 };
 
 // called by https when the request is made.
-const callback = function () {
+const callback = function (response) {
   console.log('In response handler callback!');
+  console.log('Response: ', response);
 };
 
 console.log("I'm about to make the request!");
